79e6d3eb45ae3cc3d54597eb8475c76b7853ccfc
Shalev Sharabi <ssharabi@cyberark.com>
2022-02-13 16:49:50 +0200

    Adding Box and Flex

--- src/App.tsx
+++ src/App.tsx
@@ -9,35 +9,18 @@ import Flex from "./share/Layout/Flex/Flex";
 
 const App = () => {
   return (
-    <Flex
-      background="#000"
-      zIndex={3}
-      flexDirection="row"
-      flexWrap="wrap"
-      position="fixed"
-      top={50}
-      left={50}
-      alignItems="center"
-      overflow="hidden"
-      justifyContent="center"
-      gap="30px"
-      m={0}
-      p={0}
-    >
-      <h2 style={{ color: "red" }}>Hello</h2>
-    </Flex>
-    // <Router>
-    //   <Provider store={store}>
-    //     {console.log(store.getState().users.queries)}
-    //     <Layout>
-    //       <Routes>
-    //         <Route path="/" element={<DetailTable />} />
-    //         <Route path="/todo" element={<Todos />} />
-    //         <Route path="*" element={<DetailTable />} />
-    //       </Routes>
-    //     </Layout>
-    //   </Provider>
-    // </Router>
+    <Router>
+      <Provider store={store}>
+        {console.log(store.getState().users.queries)}
+        <Layout>
+          <Routes>
+            <Route path="/" element={<DetailTable />} />
+            <Route path="/todo" element={<Todos />} />
+            <Route path="*" element={<DetailTable />} />
+          </Routes>
+        </Layout>
+      </Provider>
+    </Router>
   );
 };
 
